e568e72dabd08d5275fc26fc715733854d40e48a
Nife Sofowoke <nifemi.sofowoke@gmail.com>
2024-04-05 07:50:33 +0000

    if, elif, and else

---
+++ if-instruction-notes.py
@@ -0,0 +1,12 @@
+# computers can do things based on conditions
+# the 'if' statement is used in python for conditional statements
+
+# examples
+user_age = int(input('What is your age?'))
+if user_age > 50:
+    print('You are over 50 years old')
+    print('Sorry, you do not qualify')
+else: 
+    print('You are exactly 50 years old or younger')
+    print('Congratulations, you qualify')
+# command to be executed must be indented
